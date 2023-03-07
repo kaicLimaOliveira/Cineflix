5c83fd79fe444312a18bcceb7636bf6b5115daca
kaicLimaOliveira <kaic10@outlook.com>
2023-03-07 12:49:36 -0300

    .

--- pages/titles/src/views/handGestureView.js
+++ pages/titles/src/views/handGestureView.js
@@ -1,14 +1,15 @@
 export default class HandGestureView {
   #handsCanvas = document.querySelector('#hands')
-  #canvasContext = this.#handsCanvas.getContext('2d');
+  #canvasContext = this.#handsCanvas.getContext('2d')
   #fingerLookupIndexes
   #styler
-
   constructor({ fingerLookupIndexes, styler }) {
     this.#handsCanvas.width = globalThis.screen.availWidth
     this.#handsCanvas.height = globalThis.screen.availHeight
     this.#fingerLookupIndexes = fingerLookupIndexes
     this.#styler = styler
+
+    //  carrega os estilos assincronamente (evitar travar a tela enquanto carrega)
     setTimeout(() => styler.loadDocumentStyles(), 200);
   }
 
@@ -20,14 +21,14 @@ export default class HandGestureView {
     for (const { keypoints, handedness } of hands) {
       if (!keypoints) continue
 
-      this.#canvasContext.fillStyle = handedness === "Left" ? "rbg(44, 212, 103)" : "rbg(44, 212, 103)"
+      this.#canvasContext.fillStyle = handedness === "Left" ? "rgb(44, 212, 103)" : "rgb(44, 212, 103)"
       this.#canvasContext.strokeStyle = "white"
       this.#canvasContext.lineWidth = 8
       this.#canvasContext.lineJoin = "round"
 
       // juntas
       this.#drawJoients(keypoints)
-      // 
+      // dedos
       this.#drawFingersAndHoverElements(keypoints)
     }
   }
@@ -36,7 +37,7 @@ export default class HandGestureView {
     const element = document.elementFromPoint(x, y)
     if (!element) return;
 
-    const rect = element.getBoundingClientRect();
+    const rect = element.getBoundingClientRect()
     const event = new MouseEvent('click', {
       view: window,
       bubbles: true,
@@ -61,21 +62,19 @@ export default class HandGestureView {
       this.#canvasContext.fill()
     }
   }
-
   #drawFingersAndHoverElements(keypoints) {
     const fingers = Object.keys(this.#fingerLookupIndexes)
     for (const finger of fingers) {
       const points = this.#fingerLookupIndexes[finger].map(
         index => keypoints[index]
       )
-
       const region = new Path2D()
+      // [0] é a palma da mao (wrist)
       const [{ x, y }] = points
-
       region.moveTo(x, y)
-      for (const point of points)
+      for (const point of points) {
         region.lineTo(point.x, point.y)
-
+      }
       this.#canvasContext.stroke(region)
       this.#hoverElement(finger, points)
     }
@@ -85,11 +84,10 @@ export default class HandGestureView {
     if (finger !== "indexFinger") return
     const tip = points.find(item => item.name === "index_finger_tip")
     const element = document.elementFromPoint(tip.x, tip.y)
-
     if (!element) return;
-    const fn = () => this.#styler.toggleStyle(element, ":hover")
-
+    const fn = () => this.#styler.toggleStyle(element, ':hover')
     fn()
+
     setTimeout(() => fn(), 500);
   }
 
@@ -103,4 +101,5 @@ export default class HandGestureView {
       behavior: "smooth"
     })
   }
+
 }
